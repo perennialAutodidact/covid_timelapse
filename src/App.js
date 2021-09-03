e91e595ee565d7256c314f224765b614addc163c
unknown <keegood8@gmail.com>
2021-09-03 12:33:54 -0700

    setup arcgis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Scene } from "@esri/react-arcgis";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Scene
+        style={{ width: "100vw", height: "100vh" }}
+        mapProperties={{ basemap: "satellite" }}
+        viewProperties={{
+          center: [-122.4443, 47.2529],
+          zoom: 6,
+        }}
+      />
     </div>
   );
 }
