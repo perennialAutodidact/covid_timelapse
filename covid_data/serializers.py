76ace6445aae88ffeab3454bcde213f50d3086b6
keegangood <keegood8@gmail.com>
2021-09-09 21:42:37 -0700

    created model serializers

--- covid_data/serializers.py
+++ covid_data/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 
-from .models import CovidDatum
+from .models import Country, StateProvince, CovidDatum
 
-class CovidDatumCreateSerializer(serializers.ModelSerializer):
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+
+class StateProvinceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StateProvince
+        fields = '__all__'
+
+
+class CovidDatumSerializer(serializers.ModelSerializer):
     class Meta:
         model = CovidDatum
         fields = '__all__'
-
-    
