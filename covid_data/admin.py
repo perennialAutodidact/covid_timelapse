4f11e30d1abfe4c109f7e248b027f3cf2005e7ff
unknown <keegood8@gmail.com>
2021-09-12 18:13:45 -0700

    added sorting and searching to admin panels

--- covid_data/admin.py
+++ covid_data/admin.py
@@ -5,11 +5,13 @@ from .models import Country, StateProvince, CovidDatum
 
 class CountryAdmin(admin.ModelAdmin):
     list_display = ('name', 'latitude', 'longitude')
+    search_fields = ['name']
 
 class StateProvinceAdmin(admin.ModelAdmin):
     list_display = ('name', 'country_of_origin', 'latitude', 'longitude')
+    search_fields = ['name']
 
-class StateProvinceAdmin(admin.ModelAdmin):
+class CovidDatumAdmin(admin.ModelAdmin):
     list_display = ('name', 'latitude', 'longitude')
 
 admin.site.register(Country, CountryAdmin)
