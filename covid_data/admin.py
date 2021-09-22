8d1ac8ae9ba96f06e708424f4bb6fbe9d9e9a2fd
unknown <keegood8@gmail.com>
2021-09-22 11:53:02 -0700

    added search fields

--- covid_data/admin.py
+++ covid_data/admin.py
@@ -11,12 +11,18 @@ class StateProvinceAdmin(admin.ModelAdmin):
     list_display = ('name', 'country_of_origin', 'latitude', 'longitude')
     search_fields = ['name']
 
+
+
 class CovidDatumAdmin(admin.ModelAdmin):
-    list_display = ('name', 'latitude', 'longitude')
+    list_display = ('date', 'country', 'state_province', 'city_county')
+    search_fields = ['country__name', 'state_province__name', 'date']
+
+    # def latitude(self, instance):
+        
 
 admin.site.register(Country, CountryAdmin)
 admin.site.register(StateProvince, StateProvinceAdmin)
-admin.site.register(CovidDatum)
+admin.site.register(CovidDatum, CovidDatumAdmin)
 
 
 
