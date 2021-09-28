906d16f937721182a3026a977ffa7b0fe165e46f
unknown <keegood8@gmail.com>
2021-09-27 22:01:37 -0700

    added coordinates

---
+++ src/app/resources/countryCoordinates.js
@@ -0,0 +1,978 @@
+export default {
+  Albania: {
+    latitude: 41,
+    longitude: 20,
+  },
+  Algeria: {
+    latitude: 28,
+    longitude: 3,
+  },
+  "American Samoa": {
+    latitude: -14.3333,
+    longitude: -170,
+  },
+  Andorra: {
+    latitude: 42.5,
+    longitude: 1.6,
+  },
+  Angola: {
+    latitude: -12.5,
+    longitude: 18.5,
+  },
+  Anguilla: {
+    latitude: 18.25,
+    longitude: -63.1667,
+  },
+  Antarctica: {
+    latitude: -90,
+    longitude: 0,
+  },
+  "Antigua and Barbuda": {
+    latitude: 17.05,
+    longitude: -61.8,
+  },
+  Argentina: {
+    latitude: -34,
+    longitude: -64,
+  },
+  Armenia: {
+    latitude: 40,
+    longitude: 45,
+  },
+  Aruba: {
+    latitude: 12.5,
+    longitude: -69.9667,
+  },
+  Australia: {
+    latitude: -27,
+    longitude: 133,
+  },
+  Austria: {
+    latitude: 47.3333,
+    longitude: 13.3333,
+  },
+  Azerbaijan: {
+    latitude: 40.5,
+    longitude: 47.5,
+  },
+  Bahamas: {
+    latitude: 24.25,
+    longitude: -76,
+  },
+  Bahrain: {
+    latitude: 26,
+    longitude: 50.55,
+  },
+  Bangladesh: {
+    latitude: 24,
+    longitude: 90,
+  },
+  Barbados: {
+    latitude: 13.1667,
+    longitude: -59.5333,
+  },
+  Belarus: {
+    latitude: 53,
+    longitude: 28,
+  },
+  Belgium: {
+    latitude: 50.8333,
+    longitude: 4,
+  },
+  Belize: {
+    latitude: 17.25,
+    longitude: -88.75,
+  },
+  Benin: {
+    latitude: 9.5,
+    longitude: 2.25,
+  },
+  Bermuda: {
+    latitude: 32.3333,
+    longitude: -64.75,
+  },
+  Bhutan: {
+    latitude: 27.5,
+    longitude: 90.5,
+  },
+  Bolivia: {
+    latitude: -17,
+    longitude: -65,
+  },
+  "Bosnia and Herzegovina": {
+    latitude: 44,
+    longitude: 18,
+  },
+  Botswana: {
+    latitude: -22,
+    longitude: 24,
+  },
+  "Bouvet Island": {
+    latitude: -54.4333,
+    longitude: 3.4,
+  },
+  Brazil: {
+    latitude: -10,
+    longitude: -55,
+  },
+  "British Indian Ocean Territory": {
+    latitude: -6,
+    longitude: 71.5,
+  },
+  Brunei: {
+    latitude: 4.5,
+    longitude: 114.6667,
+  },
+  Bulgaria: {
+    latitude: 43,
+    longitude: 25,
+  },
+  "Burkina Faso": {
+    latitude: 13,
+    longitude: -2,
+  },
+  Burundi: {
+    latitude: -3.5,
+    longitude: 30,
+  },
+  Cambodia: {
+    latitude: 13,
+    longitude: 105,
+  },
+  Cameroon: {
+    latitude: 6,
+    longitude: 12,
+  },
+  Canada: {
+    latitude: 60,
+    longitude: -95,
+  },
+  "Cape Verde": {
+    latitude: 16,
+    longitude: -24,
+  },
+  "Cayman Islands": {
+    latitude: 19.5,
+    longitude: -80.5,
+  },
+  "Central African Republic": {
+    latitude: 7,
+    longitude: 21,
+  },
+  Chad: {
+    latitude: 15,
+    longitude: 19,
+  },
+  Chile: {
+    latitude: -30,
+    longitude: -71,
+  },
+  China: {
+    latitude: 35,
+    longitude: 105,
+  },
+  "Christmas Island": {
+    latitude: -10.5,
+    longitude: 105.6667,
+  },
+  "Cocos (Keeling) Islands": {
+    latitude: -12.5,
+    longitude: 96.8333,
+  },
+  Colombia: {
+    latitude: 4,
+    longitude: -72,
+  },
+  Comoros: {
+    latitude: -12.1667,
+    longitude: 44.25,
+  },
+  "Congo (Brazzaville)": {
+    latitude: -1,
+    longitude: 15,
+  },
+  "Congo (Kinshasa)": {
+    latitude: 0,
+    longitude: 25,
+  },
+  "Cook Islands": {
+    latitude: -21.2333,
+    longitude: -159.7667,
+  },
+  "Costa Rica": {
+    latitude: 10,
+    longitude: -84,
+  },
+  "C\u00f4te d'Ivoire": {
+    latitude: 8,
+    longitude: -5,
+  },
+  Croatia: {
+    latitude: 45.1667,
+    longitude: 15.5,
+  },
+  Cuba: {
+    latitude: 21.5,
+    longitude: -80,
+  },
+  Cyprus: {
+    latitude: 35,
+    longitude: 33,
+  },
+  Czechia: {
+    latitude: 49.75,
+    longitude: 15.5,
+  },
+  Denmark: {
+    latitude: 56,
+    longitude: 10,
+  },
+  Djibouti: {
+    latitude: 11.5,
+    longitude: 43,
+  },
+  Dominica: {
+    latitude: 15.4167,
+    longitude: -61.3333,
+  },
+  "Dominican Republic": {
+    latitude: 19,
+    longitude: -70.6667,
+  },
+  Ecuador: {
+    latitude: -2,
+    longitude: -77.5,
+  },
+  Egypt: {
+    latitude: 27,
+    longitude: 30,
+  },
+  "El Salvador": {
+    latitude: 13.8333,
+    longitude: -88.9167,
+  },
+  "Equatorial Guinea": {
+    latitude: 2,
+    longitude: 10,
+  },
+  Eritrea: {
+    latitude: 15,
+    longitude: 39,
+  },
+  Estonia: {
+    latitude: 59,
+    longitude: 26,
+  },
+  Ethiopia: {
+    latitude: 8,
+    longitude: 38,
+  },
+  "Falkland Islands (Malvinas)": {
+    latitude: -51.75,
+    longitude: -59,
+  },
+  "Faroe Islands": {
+    latitude: 62,
+    longitude: -7,
+  },
+  Fiji: {
+    latitude: -18,
+    longitude: 175,
+  },
+  Finland: {
+    latitude: 64,
+    longitude: 26,
+  },
+  France: {
+    latitude: 46,
+    longitude: 2,
+  },
+  "French Guiana": {
+    latitude: 4,
+    longitude: -53,
+  },
+  "French Polynesia": {
+    latitude: -15,
+    longitude: -140,
+  },
+  "French Southern Territories": {
+    latitude: -43,
+    longitude: 67,
+  },
+  Gabon: {
+    latitude: -1,
+    longitude: 11.75,
+  },
+  Gambia: {
+    latitude: 13.4667,
+    longitude: -16.5667,
+  },
+  Georgia: {
+    latitude: 42,
+    longitude: 43.5,
+  },
+  Germany: {
+    latitude: 51,
+    longitude: 9,
+  },
+  Ghana: {
+    latitude: 8,
+    longitude: -2,
+  },
+  Gibraltar: {
+    latitude: 36.1833,
+    longitude: -5.3667,
+  },
+  Greece: {
+    latitude: 39,
+    longitude: 22,
+  },
+  Greenland: {
+    latitude: 72,
+    longitude: -40,
+  },
+  Grenada: {
+    latitude: 12.1167,
+    longitude: -61.6667,
+  },
+  Guadeloupe: {
+    latitude: 16.25,
+    longitude: -61.5833,
+  },
+  Guam: {
+    latitude: 13.4667,
+    longitude: 144.7833,
+  },
+  Guatemala: {
+    latitude: 15.5,
+    longitude: -90.25,
+  },
+  Guernsey: {
+    latitude: 49.5,
+    longitude: -2.56,
+  },
+  Guinea: {
+    latitude: 11,
+    longitude: -10,
+  },
+  "Guinea-Bissau": {
+    latitude: 12,
+    longitude: -15,
+  },
+  Guyana: {
+    latitude: 5,
+    longitude: -59,
+  },
+  Haiti: {
+    latitude: 19,
+    longitude: -72.4167,
+  },
+  "Heard Island and McDonald Islands": {
+    latitude: -53.1,
+    longitude: 72.5167,
+  },
+  "Holy See": {
+    latitude: 41.9,
+    longitude: 12.45,
+  },
+  Honduras: {
+    latitude: 15,
+    longitude: -86.5,
+  },
+  "Hong Kong": {
+    latitude: 22.25,
+    longitude: 114.1667,
+  },
+  Hungary: {
+    latitude: 47,
+    longitude: 20,
+  },
+  Iceland: {
+    latitude: 65,
+    longitude: -18,
+  },
+  India: {
+    latitude: 20,
+    longitude: 77,
+  },
+  Indonesia: {
+    latitude: -5,
+    longitude: 120,
+  },
+  Iran: {
+    latitude: 32,
+    longitude: 53,
+  },
+  Iraq: {
+    latitude: 33,
+    longitude: 44,
+  },
+  Ireland: {
+    latitude: 53,
+    longitude: -8,
+  },
+  "Isle of Man": {
+    latitude: 54.23,
+    longitude: -4.55,
+  },
+  Israel: {
+    latitude: 31.5,
+    longitude: 34.75,
+  },
+  Italy: {
+    latitude: 42.8333,
+    longitude: 12.8333,
+  },
+  Jamaica: {
+    latitude: 18.25,
+    longitude: -77.5,
+  },
+  Japan: {
+    latitude: 36,
+    longitude: 138,
+  },
+  Jersey: {
+    latitude: 49.21,
+    longitude: -2.13,
+  },
+  Jordan: {
+    latitude: 31,
+    longitude: 36,
+  },
+  Kazakhstan: {
+    latitude: 48,
+    longitude: 68,
+  },
+  Kenya: {
+    latitude: 1,
+    longitude: 38,
+  },
+  Kiribati: {
+    latitude: 1.4167,
+    longitude: 173,
+  },
+  "Korea, South": {
+    latitude: 40,
+    longitude: 127,
+  },
+  "Korea, North": {
+    latitude: 37,
+    longitude: 127.5,
+  },
+  Kosovo: {
+    latitude:42.602636,
+    longitude: 20.902977
+  },
+  Kuwait: {
+    latitude: 29.3375,
+    longitude: 47.6581,
+  },
+  Kyrgyzstan: {
+    latitude: 41,
+    longitude: 75,
+  },
+  Laos: {
+    latitude: 18,
+    longitude: 105,
+  },
+  Latvia: {
+    latitude: 57,
+    longitude: 25,
+  },
+  Lebanon: {
+    latitude: 33.8333,
+    longitude: 35.8333,
+  },
+  Lesotho: {
+    latitude: -29.5,
+    longitude: 28.5,
+  },
+  Liberia: {
+    latitude: 6.5,
+    longitude: -9.5,
+  },
+  Libya: {
+    latitude: 25,
+    longitude: 17,
+  },
+  Liechtenstein: {
+    latitude: 47.1667,
+    longitude: 9.5333,
+  },
+  Lithuania: {
+    latitude: 56,
+    longitude: 24,
+  },
+  Luxembourg: {
+    latitude: 49.75,
+    longitude: 6.1667,
+  },
+  Macau: {
+    latitude: 22.1667,
+    longitude: 113.55,
+  },
+  "Macedonia, the former Yugoslav Republic of": {
+    latitude: 41.8333,
+    longitude: 22,
+  },
+  Madagascar: {
+    latitude: -20,
+    longitude: 47,
+  },
+  Malawi: {
+    latitude: -13.5,
+    longitude: 34,
+  },
+  Malaysia: {
+    latitude: 2.5,
+    longitude: 112.5,
+  },
+  Maldives: {
+    latitude: 3.25,
+    longitude: 73,
+  },
+  Mali: {
+    latitude: 17,
+    longitude: -4,
+  },
+  Malta: {
+    latitude: 35.8333,
+    longitude: 14.5833,
+  },
+  "Marshall Islands": {
+    latitude: 9,
+    longitude: 168,
+  },
+  Martinique: {
+    latitude: 14.6667,
+    longitude: -61,
+  },
+  Mauritania: {
+    latitude: 20,
+    longitude: -12,
+  },
+  Mauritius: {
+    latitude: -20.2833,
+    longitude: 57.55,
+  },
+  Mayotte: {
+    latitude: -12.8333,
+    longitude: 45.1667,
+  },
+  Mexico: {
+    latitude: 23,
+    longitude: -102,
+  },
+  Micronesia: {
+    latitude: 6.9167,
+    longitude: 158.25,
+  },
+  Moldova: {
+    latitude: 47,
+    longitude: 29,
+  },
+  Monaco: {
+    latitude: 43.7333,
+    longitude: 7.4,
+  },
+  Mongolia: {
+    latitude: 46,
+    longitude: 105,
+  },
+  Montenegro: {
+    latitude: 42,
+    longitude: 19,
+  },
+  Montserrat: {
+    latitude: 16.75,
+    longitude: -62.2,
+  },
+  Morocco: {
+    latitude: 32,
+    longitude: -5,
+  },
+  Mozambique: {
+    latitude: -18.25,
+    longitude: 35,
+  },
+  Myanmar: {
+    latitude: 22,
+    longitude: 98,
+  },
+  Namibia: {
+    latitude: -22,
+    longitude: 17,
+  },
+  Nauru: {
+    latitude: -0.5333,
+    longitude: 166.9167,
+  },
+  Nepal: {
+    latitude: 28,
+    longitude: 84,
+  },
+  Netherlands: {
+    latitude: 52.5,
+    longitude: 5.75,
+  },
+  "Netherlands Antilles": {
+    latitude: 12.25,
+    longitude: -68.75,
+  },
+  "New Caledonia": {
+    latitude: -21.5,
+    longitude: 165.5,
+  },
+  "New Zealand": {
+    latitude: -41,
+    longitude: 174,
+  },
+  Nicaragua: {
+    latitude: 13,
+    longitude: -85,
+  },
+  Niger: {
+    latitude: 16,
+    longitude: 8,
+  },
+  Nigeria: {
+    latitude: 10,
+    longitude: 8,
+  },
+  Niue: {
+    latitude: -19.0333,
+    longitude: -169.8667,
+  },
+  "Norfolk Island": {
+    latitude: -29.0333,
+    longitude: 167.95,
+  },
+  "Northern Mariana Islands": {
+    latitude: 15.2,
+    longitude: 145.75,
+  },
+  Norway: {
+    latitude: 62,
+    longitude: 10,
+  },
+  Oman: {
+    latitude: 21,
+    longitude: 57,
+  },
+  Pakistan: {
+    latitude: 30,
+    longitude: 70,
+  },
+  Palau: {
+    latitude: 7.5,
+    longitude: 134.5,
+  },
+  "Palestinian Territory, Occupied": {
+    latitude: 32,
+    longitude: 35.25,
+  },
+  Panama: {
+    latitude: 9,
+    longitude: -80,
+  },
+  "Papua New Guinea": {
+    latitude: -6,
+    longitude: 147,
+  },
+  Paraguay: {
+    latitude: -23,
+    longitude: -58,
+  },
+  Peru: {
+    latitude: -10,
+    longitude: -76,
+  },
+  Philippines: {
+    latitude: 13,
+    longitude: 122,
+  },
+  Pitcairn: {
+    latitude: -24.7,
+    longitude: -127.4,
+  },
+  Poland: {
+    latitude: 52,
+    longitude: 20,
+  },
+  Portugal: {
+    latitude: 39.5,
+    longitude: -8,
+  },
+  "Puerto Rico": {
+    latitude: 18.25,
+    longitude: -66.5,
+  },
+  Qatar: {
+    latitude: 25.5,
+    longitude: 51.25,
+  },
+  "R\u00e9union": {
+    latitude: -21.1,
+    longitude: 55.6,
+  },
+  Romania: {
+    latitude: 46,
+    longitude: 25,
+  },
+  Russia: {
+    latitude: 60,
+    longitude: 100,
+  },
+  Rwanda: {
+    latitude: -2,
+    longitude: 30,
+  },
+  "Saint Helena, Ascension and Tristan da Cunha": {
+    latitude: -15.9333,
+    longitude: -5.7,
+  },
+  "Saint Kitts and Nevis": {
+    latitude: 17.3333,
+    longitude: -62.75,
+  },
+  "Saint Lucia": {
+    latitude: 13.8833,
+    longitude: -61.1333,
+  },
+  "Saint Pierre and Miquelon": {
+    latitude: 46.8333,
+    longitude: -56.3333,
+  },
+  "Saint Vincent and the Grenadines": {
+    latitude: 13.25,
+    longitude: -61.2,
+  },
+  Samoa: {
+    latitude: -13.5833,
+    longitude: -172.3333,
+  },
+  "San Marino": {
+    latitude: 43.7667,
+    longitude: 12.4167,
+  },
+  "Sao Tome and Principe": {
+    latitude: 1,
+    longitude: 7,
+  },
+  "Saudi Arabia": {
+    latitude: 25,
+    longitude: 45,
+  },
+  Senegal: {
+    latitude: 14,
+    longitude: -14,
+  },
+  Serbia: {
+    latitude: 44,
+    longitude: 21,
+  },
+  Seychelles: {
+    latitude: -4.5833,
+    longitude: 55.6667,
+  },
+  "Sierra Leone": {
+    latitude: 8.5,
+    longitude: -11.5,
+  },
+  Singapore: {
+    latitude: 1.3667,
+    longitude: 103.8,
+  },
+  Slovakia: {
+    latitude: 48.6667,
+    longitude: 19.5,
+  },
+  Slovenia: {
+    latitude: 46,
+    longitude: 15,
+  },
+  "Solomon Islands": {
+    latitude: -8,
+    longitude: 159,
+  },
+  Somalia: {
+    latitude: 10,
+    longitude: 49,
+  },
+  "South Africa": {
+    latitude: -29,
+    longitude: 24,
+  },
+  "South Georgia and the South Sandwich Islands": {
+    latitude: -54.5,
+    longitude: -37,
+  },
+  Spain: {
+    latitude: 40,
+    longitude: -4,
+  },
+  "Sri Lanka": {
+    latitude: 7,
+    longitude: 81,
+  },
+  Sudan: {
+    latitude: 15,
+    longitude: 30,
+  },
+  Suriname: {
+    latitude: 4,
+    longitude: -56,
+  },
+  "Svalbard and Jan Mayen": {
+    latitude: 78,
+    longitude: 20,
+  },
+  Swaziland: {
+    latitude: -26.5,
+    longitude: 31.5,
+  },
+  Sweden: {
+    latitude: 62,
+    longitude: 15,
+  },
+  Switzerland: {
+    latitude: 47,
+    longitude: 8,
+  },
+  Syria: {
+    latitude: 35,
+    longitude: 38,
+  },
+  Taiwan: {
+    latitude: 23.5,
+    longitude: 121,
+  },
+  Tajikistan: {
+    latitude: 39,
+    longitude: 71,
+  },
+  Tanzania: {
+    latitude: -6,
+    longitude: 35,
+  },
+  Thailand: {
+    latitude: 15,
+    longitude: 100,
+  },
+  "Timor-Leste": {
+    latitude: -8.55,
+    longitude: 125.5167,
+  },
+  Togo: {
+    latitude: 8,
+    longitude: 1.1667,
+  },
+  Tokelau: {
+    latitude: -9,
+    longitude: -172,
+  },
+  Tonga: {
+    latitude: -20,
+    longitude: -175,
+  },
+  "Trinidad and Tobago": {
+    latitude: 11,
+    longitude: -61,
+  },
+  Tunisia: {
+    latitude: 34,
+    longitude: 9,
+  },
+  Turkey: {
+    latitude: 39,
+    longitude: 35,
+  },
+  Turkmenistan: {
+    latitude: 40,
+    longitude: 60,
+  },
+  "Turks and Caicos Islands": {
+    latitude: 21.75,
+    longitude: -71.5833,
+  },
+  Tuvalu: {
+    latitude: -8,
+    longitude: 178,
+  },
+  Uganda: {
+    latitude: 1,
+    longitude: 32,
+  },
+  Ukraine: {
+    latitude: 49,
+    longitude: 32,
+  },
+  "United Arab Emirates": {
+    latitude: 24,
+    longitude: 54,
+  },
+  "United Kingdom": {
+    latitude: 54,
+    longitude: -2,
+  },
+  "United States": {
+    latitude: 38,
+    longitude: -97,
+  },
+  "United States Minor Outlying Islands": {
+    latitude: 19.2833,
+    longitude: 166.6,
+  },
+  Uruguay: {
+    latitude: -33,
+    longitude: -56,
+  },
+  Uzbekistan: {
+    latitude: 41,
+    longitude: 64,
+  },
+  Vanuatu: {
+    latitude: -16,
+    longitude: 167,
+  },
+  Venezuela: {
+    latitude: 8,
+    longitude: -66,
+  },
+  "Viet Nam": {
+    latitude: 16,
+    longitude: 106,
+  },
+  "Virgin Islands, British": {
+    latitude: 18.5,
+    longitude: -64.5,
+  },
+  "Virgin Islands, U.S.": {
+    latitude: 18.3333,
+    longitude: -64.8333,
+  },
+  "Wallis and Futuna": {
+    latitude: -13.3,
+    longitude: -176.2,
+  },
+  "Western Sahara": {
+    latitude: 24.5,
+    longitude: -13,
+  },
+  Yemen: {
+    latitude: 15,
+    longitude: 48,
+  },
+  Zambia: {
+    latitude: -15,
+    longitude: 30,
+  },
+  Zimbabwe: {
+    latitude: -20,
+    longitude: 30,
+  },
+  Afghanistan: {
+    latitude: 33,
+    longitude: 65,
+  },
+};
