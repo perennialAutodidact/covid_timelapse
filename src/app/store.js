4ed2750cdeb6c0988133b0f8deb45afc259ed82c
unknown <keegood8@gmail.com>
2021-11-29 23:26:16 -0800

    added state and action devtools sanitizers

--- src/app/store.js
+++ src/app/store.js
@@ -1,23 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import covidReducer from "../app/slices/covidSlice";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from 'redux-thunk';
 
-// const stateSanitizer = (state) => state.covidData.data ? { ...state.covidData, data: '<<LONG_BLOB>>' } : state.covidData
+const stateSanitizer = (state) => state.covidData.dataChunks ? { ...state.covidData, dataChunks: '<<LONG_BLOB>>' } : state.covidData
+const actionSanitizer = (action) => (
+  action.type === 'ADD_DATA_CHUNK' && action.data ?
+  { ...action, data: '<<LONG_BLOB>>' } : action
+);
 
 
 export default configureStore({
   reducer: {
     covidData: covidReducer,
   },
-  devTools: true,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false
   }),
-  
-  // enhancers: defaultEnhancers => [stateSanitizer, ...defaultEnhancers],
-  // middleware: getDefaultMiddleware => 
-  //   getDefaultMiddleware({
-  //     serializableCheck: false
-  //   })
+  devTools: {
+    stateSanitizer: stateSanitizer,
+    actionSanitizer: actionSanitizer
+  }
 });
